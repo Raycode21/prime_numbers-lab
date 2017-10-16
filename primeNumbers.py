1e83079b4629668112f6415207d2dc52c8207b23
raycode21 <wanjiku.rachel21@gmail.com>
2017-10-16 16:32:04 +0300

    in sync with test

--- primeNumbers.py
+++ primeNumbers.py
@@ -1,12 +1,16 @@
-myPrimeList =[]
-min_num = 0
-max_num = int(input("Enter your the number within which you want prime numbers:"))
-for x in range (min_num,max_num+1):
-  if x > 1:
-       for y in range(2,x):
-           if (x % y) == 0:
-               break
-       else:
-           myPrimeList.append(x)
-print(myPrimeList)
-      
+
+def primeNumbers(num):
+    myPrimeList = []
+    for x in range (num+1):
+        if x > 1:
+            for y in range(2,x):
+                if (x % y) == 0:
+                    break
+            else:
+                myPrimeList.append(x)
+    return myPrimeList
+
+def enterNumber():
+	max_num = int(input("Enter your the number within which you want prime numbers:"))
+	return max_num
+    
